ef12beb427bf129c772585bf39efd2cd4e8bc0c8
John Haupenthal <john.haupenthal@vincit.com>
2019-11-16 17:45:34 -0800

    squash.

--- install-git-master-of-metal.sh
+++ install-git-master-of-metal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script was copied with love from https://gist.githubusercontent.com/stefansundin/d465f1e331fc5c632088/raw/install-pre-push.sh 
 
-# curl -fL https://github.com/johnhaup/git-master-of-metal/raw/install-pre-push.sh | sh
+# curl -fL https://raw.githubusercontent.com/johnhaup/git-master-of-metal/master/install-git-master-of-metal.sh | sh
 # Uninstall:
 # rm .git/hooks/pre-push
 
@@ -26,7 +26,7 @@ fi
 echo Downloading pre-push hook from https://github.com/johnhaup/git-master-of-metal
 echo
 
-curl -fL -o "$GIT_DIR/hooks/pre-push" "https://github.com/johnhaup/git-master-of-metal/pre-push"
+curl -fL -o "$GIT_DIR/hooks/pre-push" "https://raw.githubusercontent.com/johnhaup/git-master-of-metal/master/pre-push"
 if [ ! -f "$GIT_DIR/hooks/pre-push" ]; then
 	echo Error downloading pre-push script!
 	exit 3
